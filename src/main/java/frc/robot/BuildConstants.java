480bb53271fdee36d55ffb5fb673b50c2c2b50a1
LiamDark-Z <99617804+LiamDark-Z@users.noreply.github.com>
2026-02-06 19:23:22 -0600

    Update BuildConstants.java

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "RR-2026-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 2;
-  public static final String GIT_SHA = "835e9fb39c35ce25e341bad94b0d1876016998f5";
-  public static final String GIT_DATE = "2026-02-01 12:44:30 EST";
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "b69fe16b7d0dbd43273428986e1e41a63776c4f8";
+  public static final String GIT_DATE = "2026-02-01 14:50:14 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-02-01 12:46:35 EST";
-  public static final long BUILD_UNIX_TIME = 1769967995050L;
+  public static final String BUILD_DATE = "2026-02-01 15:07:35 EST";
+  public static final long BUILD_UNIX_TIME = 1769976455332L;
   public static final int DIRTY = 0;
 
   private BuildConstants() {}
